46f4ca0a68f60e13d62de3379f263b3342fa21f6
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-08-05 10:56:31 +0100

    BAVL-1435 resolve issue with add another component. Bug likely related to earlier frontend updates. (#378)

--- frontend/components/moj-add-another/moj-add-another.js
+++ frontend/components/moj-add-another/moj-add-another.js
@@ -1,21 +1,35 @@
 function MojAddAnother($component) {
+  // Store original prototype methods if needed
+  const originalFocusHeading = MOJFrontend.AddAnother.prototype.focusHeading
+  const originalCreateRemoveButton = MOJFrontend.AddAnother.prototype.createRemoveButton
+  const originalGetNewItem = MOJFrontend.AddAnother.prototype.getNewItem
+
   // Our component does not have a heading, so we override this function to do nothing
-  MOJFrontend.AddAnother.prototype.focusHeading = () => {}
+  MOJFrontend.AddAnother.prototype.focusHeading = function () {}
 
   // Our component's remove button has different classes to the original, so we override this function
-  MOJFrontend.AddAnother.prototype.createRemoveButton = (item) => item.append('<button type="button" class="moj-add-another__remove-button">Remove</button>');
+  MOJFrontend.AddAnother.prototype.createRemoveButton = function (item) {
+    return item.append('<button type="button" class="moj-add-another__remove-button">Remove</button>')
+  }
 
   // The original component clones the first element when clicking "Add another", including the value of that element, which we want to leave blank
   MOJFrontend.AddAnother.prototype.getNewItem = function () {
-    const item = this.getItems().first().clone();
+    const item = this.getItems().first().clone()
     if (!this.hasRemoveButton(item)) {
-      this.createRemoveButton(item);
+      this.createRemoveButton(item)
     }
     item.find('input, textarea, select').val('')
     return item
-  };
+  }
 
-  new MOJFrontend.AddAnother($component)
+  const instance = new MOJFrontend.AddAnother($component)
+
+  // Restore original prototype methods to avoid affecting other instances
+  MOJFrontend.AddAnother.prototype.focusHeading = originalFocusHeading
+  MOJFrontend.AddAnother.prototype.createRemoveButton = originalCreateRemoveButton
+  MOJFrontend.AddAnother.prototype.getNewItem = originalGetNewItem
+
+  return instance
 }
 
 export default MojAddAnother
